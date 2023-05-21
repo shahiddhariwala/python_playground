8c4caa44585ae9809d9ed4341affdcac40e46450
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-05-21 16:00:50 +0530

    List basics

---
+++ list_basic.py
@@ -0,0 +1,48 @@
+states = [
+    "Andhra Pradesh",
+    "Arunachal Pradesh",
+    "Assam",
+    "Bihar",
+    "Chhattisgarh",
+    "Goa",
+    "Gujarat",
+    "Haryana",
+    "Himachal Pradesh",
+    "Jammu and Kashmir",
+    "Jharkhand",
+    "Karnataka",
+    "Kerala",
+    "Lakshadweep",
+    "Madhya Pradesh",
+    "Maharashtra",
+    "Manipur",
+    "Meghalaya",
+    "Mizoram",
+    "Nagaland",
+    "Odisha",
+    "Punjab",
+    "Rajasthan",
+    "Sikkim",
+    "Tamil Nadu",
+    "Telangana",
+    "Tripura",
+    "Uttar Pradesh",
+    "Uttarakhand",
+    "West Bengal",
+    "Andaman and Nicobar Islands",
+    "Chandigarh",
+    "Dadra and Nagar Haveli",
+    "Daman and Diu",
+    "Lakshadweep",
+    "Puducherry"
+]
+
+
+print(states[0])
+print(states[-1])
+states.append("ShahidLand")
+print(states[-1])
+states.remove(states[-1])
+print(states[-1])
+states.extend(["Pokemon", "Bleach"])
+print(states[-3:])
