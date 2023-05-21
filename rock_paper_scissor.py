e54771b0da5a774b0f352cd08a8903da5fe457d6
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-05-21 17:05:44 +0530

    Added try catch to rock paper scissor game

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -40,47 +40,47 @@ choices_art = {
     "paper": paper,
     "scissors": scissors
 }
-user_choice = choices[int(
-    input("What do you choose ? Type 0 for Rock, 1 for Paper or 2 for Scissors\n")) % 3]
 
+try:
+    user_choice = choices[int(
+        input("What do you choose ? Type 0 for Rock, 1 for Paper or 2 for Scissors\n")) % 3]
 
-def generate_computer_choice():
-    return choices[random.randint(0, 2)]
+    def generate_computer_choice():
+        return choices[random.randint(0, 2)]
 
+    computer_choice = generate_computer_choice()
 
-computer_choice = generate_computer_choice()
+    def check_who_won(user_c, computer_c):
+        # Rules
+        if (user_c == computer_c):
+            winner = "draw"
+        elif user_c == "rock":
+            if computer_c == "scissors":
+                winner = "user"
+            else:
+                winner = "computer"
+        elif user_c == "paper":
+            if computer_c == "rock":
+                winner = "user"
+            else:
+                winner = "computer"
+        elif user_c == "scissors":
+            if computer_c == "paper":
+                winner = "user"
+            else:
+                winner = "computer"
+        return winner
 
+    print(choices_art[user_choice])
 
-def check_who_won(user_c, computer_c):
-    # Rules
-    if (user_c == computer_c):
-        winner = "draw"
-    elif user_c == "rock":
-        if computer_c == "scissors":
-            winner = "user"
-        else:
-            winner = "computer"
-    elif user_c == "paper":
-        if computer_c == "rock":
-            winner = "user"
-        else:
-            winner = "computer"
-    elif user_c == "scissors":
-        if computer_c == "paper":
-            winner = "user"
-        else:
-            winner = "computer"
-    return winner
+    print(f"Computer Choose:\n{choices_art[computer_choice]}")
 
-
-print(choices_art[user_choice])
-
-print(f"Computer Choose:\n{choices_art[computer_choice]}")
-
-winner = check_who_won(user_choice, computer_choice)
-if winner == "user":
-    print("You won")
-elif winner == "computer":
-    print("You lost")
-else:
-    print("Its draw")
+    winner = check_who_won(user_choice, computer_choice)
+    if winner == "user":
+        print("You won")
+    elif winner == "computer":
+        print("You lost")
+    else:
+        print("Its draw")
+except:
+    print("Oho sdomething went wrong! Please check your inputs")
