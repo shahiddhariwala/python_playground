af4c469c8b097642c24d3cd1a0d53888e2ffafe4
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-05-23 23:23:32 +0530

    Secret auction dict basic

---
+++ secret_auction.py
@@ -0,0 +1,33 @@
+auction_bid_art = '''
+                __
+               (_()  \\
+          \__/  ||    \  \__/
+          (oo)  /)       (oo)
+         //~~\\//       //~~\\
+         \\__/\/   _____\\__//_____
+          |/\|    |                |
+    _____ |||| ___|                |______
+   ______(_)(_)___|________________|_______
+
+'''
+
+print(auction_bid_art)
+
+auction_ledger = {}
+continue_bidding = True
+
+while continue_bidding:
+    name = input("What is your name?\n")
+    bid = int(input("What is your bid $"))
+    auction_ledger[name] = bid
+    if input(
+            "Are there any other bidders, type 'Yes' or 'No'\n").lower() == "no":
+        continue_bidding = False
+winner = ""
+winning_bid = 0
+for bidder in auction_ledger:
+    if winning_bid < auction_ledger[bidder]:
+        winning_bid = auction_ledger[bidder]
+        winner = bidder
+
+print(f"Winner is {winner} with bid of ${winning_bid}")
