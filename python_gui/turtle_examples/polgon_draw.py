3ceeb7de980e0dfdcb6ef43240598c258dbccb6d
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-05-27 21:37:31 +0530

    Drawing Polygon

---
+++ python_gui/turtle_examples/polgon_draw.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+import random as r
+
+inny = Turtle()
+screen = Screen()
+side = 3
+colors = ["red", "yellow", "purple", "green", "violet", "orange", "pink", "wheat", "gray"]
+
+
+def draw_polygon(num_sides):
+    inny.color(r.choice(colors))
+    angle = 360 / side
+    for _ in range(side):
+        inny.forward(100)
+        inny.right(angle)
+
+
+for num_sides in range(3, 11):
+    draw_polygon(num_sides)
+    side += 1
+
+screen.exitonclick()
