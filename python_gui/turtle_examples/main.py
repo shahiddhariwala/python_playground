8ec72c520ecde931edba25b5c2f11895bf734cd6
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-05-27 21:25:16 +0530

    Basics drawing paradigms

--- python_gui/turtle_examples/main.py
+++ python_gui/turtle_examples/main.py
@@ -1,13 +1,26 @@
 from turtle import Turtle, Screen
 
 inny = Turtle()
+screen = Screen()
+screen.title("Inny Playground!")
 inny.shape("turtle")
 # Drawing Circle
 inny.color("purple")
 inny.circle(200, 360)
 inny.shape("circle")
-inny.color("red")
-inny.circle(300, 360)
 
-screen = Screen()
+# Drawing Square
+for _ in range(4):
+    inny.forward(100)
+    inny.right(90)
+
+# Drawing Dashed Lines
+
+for num in range(30):
+    if num % 2 == 0:
+        inny.penup()
+    else:
+        inny.pendown()
+    inny.forward(10)
+
 screen.exitonclick()
