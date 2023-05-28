b5e70bebd31ebe71c8a8652247f215a52e218669
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-05-28 23:27:05 +0530

    Pandas playing with dataframes and new way of generating list i.e list compression

--- weather_data/pandas_basic.py
+++ weather_data/pandas_basic.py
@@ -7,9 +7,38 @@ data_dict = data.to_dict()
 print(data_dict)
 
 # Converts to list from series
-temperatures = data["Temperature"].tolist()
-print(temperatures)
-print(f"Average temperature is {sum(temperatures) / len(temperatures)}'C")
+temperatures_series = data["Temperature"]
+temp_list = temperatures_series.tolist()
+
+print(temp_list)
+print(f"Average temperature is {sum(temp_list) / len(temp_list)}'C")
+
+# Pandas inbuilt methods
+
+# Get mean or average
+print(f"Average temperature is {temperatures_series.mean()}'C")
+
+# Get max temperature
+max_temp = temperatures_series.max()
+print(data[data.Temperature == max_temp])
+
+# Create dataframe from scratch
+
+import random
+
+# List of class names
+classes = ["1st std", "2nd std", "3rd std", "4th std", "5th std", "6th std", "7th std", "8th std", "9th std",
+           "10th std", "11th std", "12th std"]
+
+# Dictionary with class and number of students
+class_students = {
+    "class": classes,
+    "students": [random.randint(10, 30) for _ in classes]  # List compression: Generating random number of students for
+    # each class this is called list compression
+}
+
+class_data = pandas.DataFrame(class_students)
+print(class_data)
 """
 
 print(data)
