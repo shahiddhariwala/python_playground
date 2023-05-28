a8c3be6a7a50558a8175fecfdfae7b1e61851def
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-05-28 17:17:00 +0530

    Ball creations and bouncing and reset on collison missess logic added

--- python_gui/pong_arcade_game/ball.py
+++ python_gui/pong_arcade_game/ball.py
@@ -1,5 +1,7 @@
 import turtle
 
+MOVEMENT = 10
+
 
 class Ball(turtle.Turtle):
 
@@ -7,3 +9,26 @@ class Ball(turtle.Turtle):
         super().__init__()
         self.color("white")
         self.shape("circle")
+        self.setheading(45)
+        self.penup()
+        self.x_move = MOVEMENT
+        self.y_move = MOVEMENT
+
+    def move(self):
+        self.goto(self.xcor() + self.x_move, self.ycor() + self.y_move)
+
+    def bounce_y(self):
+        """
+        Reverses direction in Y axis
+        """
+        self.y_move *= -1
+
+    def bounce_x(self):
+        """
+        Reverses direction in X axis
+        """
+        self.x_move *= -1
+
+    def reset_ball(self):
+        self.bounce_x()
+        self.goto(0, 0)
--- python_gui/pong_arcade_game/main.py
+++ python_gui/pong_arcade_game/main.py
@@ -1,22 +1,42 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
-
+import time
 screen = Screen()
 screen.title("Ping Pong Game!")
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
+is_game_on = True
 player_l = Paddle((-350, 0))
 player_r = Paddle((350, 0))
 ball = Ball()
 
-screen.update()
 screen.listen()
 screen.onkey(player_r.go_up, "Up")
 screen.onkey(player_r.go_down, "Down")
 screen.onkey(player_l.go_up, "w")
 screen.onkey(player_l.go_down, "s")
+
+while is_game_on:
+
+    time.sleep(.1)
+
+    ball.move()
+    screen.update()
+
+    # Collision with top & bottom wall
+
+    if ball.ycor() >= 280 or ball.ycor() <= -280:
+        ball.bounce_y()
+
+    # Collision with paddles
+    if ball.distance(player_r) <= 50 and ball.xcor() >= 330 or ball.distance(player_l) <= 50 and ball.xcor() <= -330:
+        ball.bounce_x()
+
+    # Detect misses
+    if ball.xcor() >= 380 or ball.xcor() <= -380:
+        ball.reset_ball()
 
 screen.exitonclick()
