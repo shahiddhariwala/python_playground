f3238564912b78de6d5aedbd48284ca89c2e8250
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-05-28 16:21:33 +0530

    Refactored to create a paddle with based on postition for reusibility

--- python_gui/pong_arcade_game/paddle.py
+++ python_gui/pong_arcade_game/paddle.py
@@ -4,18 +4,14 @@ MOVEMENT = 20
 
 
 class Paddle(turtle.Turtle):
-    def __init__(self, player_number=1):
+    def __init__(self, initial_pos):
         super().__init__()
-        self.player_number = player_number
+        self.initial_pos = initial_pos
         self.color("white")
         self.shape("square")
         self.penup()
         self.shapesize(stretch_wid=5, stretch_len=1)
-        if player_number == 1:
-            self.x_pos = 350
-        else:
-            self.x_pos = -350
-        self.goto(self.x_pos, 0)
+        self.goto(initial_pos)
 
     def update_screen(self):
         screen = self.screen
@@ -24,11 +20,11 @@ class Paddle(turtle.Turtle):
     def go_up(self):
         y_pos = self.ycor()
         if y_pos < 260:
-            self.goto(self.x_pos, y_pos + MOVEMENT)
+            self.goto(self.xcor(), y_pos + MOVEMENT)
             self.update_screen()
 
     def go_down(self):
         y_pos = self.ycor()
         if y_pos > -260:
-            self.goto(self.x_pos, y_pos - MOVEMENT)
+            self.goto(self.xcor(), y_pos - MOVEMENT)
             self.update_screen()
