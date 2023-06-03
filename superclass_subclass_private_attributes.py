9a12d1f74d8d0e9012136eb2ed29c904787fd77e
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-06-03 17:40:27 +0530

    Accessing public attribute from super class can be done with name mangling

--- superclass_subclass_private_attributes.py
+++ superclass_subclass_private_attributes.py
@@ -1,6 +1,7 @@
 class Superclass:
     def __init__(self):
         self.__private_attribute = 42
+        self.public_attribute = 786
 
     def __private_method(self):
         print("This is a private method.")
@@ -9,6 +10,9 @@ class Superclass:
 class Subclass(Superclass):
     def access_private_attribute(self):
         print("Accessing private attribute:", self._Superclass__private_attribute)
+
+    def access_public_attribute(self):
+        print("Accessing public attribute:", self.public_attribute)
 
     def call_private_method(self):
         self._Superclass__private_method()
@@ -23,5 +27,7 @@ obj.access_private_attribute()  # Output: Accessing private attribute: 42
 # Call private method in the subclass
 obj.call_private_method()  # Output: This is a private method.
 
+obj.access_public_attribute()
+print(obj.public_attribute)
 # Attempt to access private attribute directly in the subclass
 # print(obj.__private_attribute)  # This will raise an AttributeError
